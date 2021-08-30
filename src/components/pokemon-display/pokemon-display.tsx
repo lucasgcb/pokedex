45ef1a5e7ebbc83813711149e74f2a8db286250f
Lucas Bandeira <Lucas Bandeira>
2021-08-30 04:09:42 -0300

    feature: Add Radio chart for stats

--- src/components/pokemon-display/pokemon-display.tsx
+++ src/components/pokemon-display/pokemon-display.tsx
@@ -6,18 +6,16 @@ import * as echarts from 'echarts'
 import './pokemon-display.css'
 
 const PokemonDisplay: FunctionComponent = () => {
-    
-    const [selectedPokemon, setSelectedPokemon] = React.useState('');
-    const [availablePokemon, setAvailablePokemon] = React.useState<PokemonListItem[]>([]);
+    const [currentImage, setCurrentImage] = React.useState('');
 
     const pokemon = useSelector((state:ApplicationState)=> state.pokemonSelected)
-    const handleChange = (event: React.ChangeEvent<{ value: string }>) => {
-      setSelectedPokemon(event.target.value);
-    };
 
     useEffect(()=>{
         if(pokemon.isFetched)
+        {
             buildStatRadarPlot('pokemon-stat-plot')
+            setCurrentImage(pokemon.picture)
+        }
     },[pokemon.isFetched])
 
     const buildStatRadarPlot = (id:string):echarts.ECharts =>{
@@ -26,19 +24,17 @@ const PokemonDisplay: FunctionComponent = () => {
           throw ('Radar Chart div disappeared')
         const myChart = echarts.init(chartDom)
         const option = {
-            title: {
-                text: '基础雷达图'
-            },
             legend: {
-                data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+                data: ["Base Attributes", ]
             },
             radar: {
                 indicator: [
-                    { name: 'HP', max: 120},
-                    { name: 'Attack', max: 120},
-                    { name: 'Special Attack', max: 120},
-                    { name: 'Special Defense', max: 120},
-                    { name: 'Speed', max: 120},
+                    { name: 'HP', max: 200},
+                    { name: 'Attack', max: 200},
+                    { name: 'Special Attack', max: 200},
+                    { name: 'Speed', max: 200},
+                    { name: 'Special Defense', max: 200},
+                    { name: 'Defense', max: 200},
                 ]
             },
             series: [{
@@ -46,8 +42,27 @@ const PokemonDisplay: FunctionComponent = () => {
                 type: 'radar',
                 data: [
                     {
-                        value: [pokemon.stats.hp, pokemon.stats.attack, pokemon.stats.specialAttack, pokemon.stats.specialDefense, pokemon.stats.speed],
-                        name: '预算分配（Allocated Budget）'
+                        value: [pokemon.stats.hp, pokemon.stats.attack, pokemon.stats.specialAttack,
+                                pokemon.stats.speed, pokemon.stats.specialDefense, 
+                                pokemon.stats.defense],
+                        name: 'Pokemon Stats',
+                        label: {
+                            show: true
+                        },
+                        symbol: 'rect',
+                        areaStyle: {
+                            color: new echarts.graphic.RadialGradient(0.1, 0.6, 1, [
+                                {
+                                    color: 'rgba(255, 145, 124, 0.1)',
+                                    offset: 0
+                                },
+                                {
+                                    color: 'rgba(255, 145, 124, 0.9)',
+                                    offset: 1
+                                }
+                            ])
+                        }
+
                     },
                 ]
             }]
@@ -57,11 +72,25 @@ const PokemonDisplay: FunctionComponent = () => {
       }  
 
     return <div id="pokemon-display-parent">
-        {pokemon.name}
-        <div id="pokemon-stat-plot">
-
+        <div id="pokemon-profile">
+            <img src={currentImage}></img>
+        </div>
+        <div id="pokemon-stat-title">
+         {`${pokemon.name} base stats`}
+        </div>
+        <div id="pokemon-stat-plot-parent">
+            <div id="pokemon-stat-plot"></div>
+        </div>
+        <div id="pokemon-curiosity">
+            Total Base Stats: {sumAllBaseStats(pokemon)} pts.
         </div>
     </div>
 }
 
 export default PokemonDisplay
+function sumAllBaseStats(pokemon: PokemonSelectedState): React.ReactNode {
+    return Object.values(pokemon.stats).reduce((total,current) => {
+        return current + total
+    })
+}
+
